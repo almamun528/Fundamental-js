d331f0ab9cf6de5e6637ea86e9238c6dce004094
almamun528 <ayapths@gmail.com>
2024-09-02 19:16:11 +0600

    array push and pop

---
+++ array.js
@@ -0,0 +1,16 @@
+var students =["Jony","Rony","Tony"]
+
+console.log(students)
+ 
+students.push("Tina", "Hena" , "Joya", "Mamun")
+
+console.log( students)
+
+
+
+students.pop("Mamun")
+
+
+console.log(  students)
+
+console.log(typeof students)
