dbc126b747b8b7366bf5b1edf4d8821390cee759
almamun528 <ayapths@gmail.com>
2024-09-05 22:43:12 +0600

    loops in array complete and practice + programming hero task is done

--- array.js
+++ array.js
@@ -22,7 +22,7 @@ console.log(  students)
 console.log("--------------------------------")
 
 
-//! Single Line of reversed array 
+// //! Single Line of reversed array 
 var newArray= ['rahim', 'karim', 'jamil', 'rony','jony' ]
 
 let ulta =newArray.reverse()
@@ -34,7 +34,7 @@ let rev_numbers= NUMBERS.reverse()
 console.log(NUMBERS)
 
 
-//! for loop reverse the array 
+// //! for loop reverse the array 
 
 var NumBers = [1 , 2, 3, 5, 6, 7, 8, 9]
 
@@ -92,3 +92,110 @@ for ( let i = values.length-1; i>=0 ; i-- ){
 
 }
 console.log(empty)
+
+/*
+// !Task 1
+// Write a JavaScript code to reverse the array colors without using the reverse method.
+
+// Input: const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+
+// Output:
+
+// ['orange', 'yellow', 'green', 'blue', 'red']
+*/
+
+var colors = ['red', 'blue', 'green', 'yellow', 'orange']
+
+var reversedColor= []
+for( let i = 0; i<colors.length; i++ ){
+
+    const changed = colors[i]
+    reversedColor.unshift(changed)
+
+}
+console.log(reversedColor)
+
+
+/*** 
+ //!Task 2
+ * Write a JavaScript code to get the "//!even numbers"  from an array using any looping technique.
+
+Input: const numbers = [12, 98, 5, 41, 23, 78, 46];
+
+Output:
+
+[12, 98, 76, 46]
+
+
+ * 
+ */
+
+var numbers  = [12, 98, 5, 41, 23, 78, 46];
+
+var emptyStore =[]
+
+
+for( let i = numbers.length-1; i>=0;i-- ){
+    const store = numbers[i]
+    
+
+    if( store % 2 == 0){
+        emptyStore.push(store)
+    }
+}
+console.log(emptyStore)
+
+/**
+//! Task 3
+Use a for...of loop to concatenate all the elements of an array into a single string.
+
+Input: var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+
+Output:
+
+'TomTimTinTik'
+*/
+
+var arr = ['Tom', 'Tim', 'Tin', 'Tik']
+
+var newarr=[]
+for(chr of arr){
+    
+}
+for ( let i=0; i<arr.length; i++){
+    const store = arr[i]
+    newarr.push(store)
+}
+console.log(newarr.join(''))
+for (let chr of arr){
+   
+    newarr.push(chr)
+    
+}
+console.log(newarr.join(""))
+
+/**
+ //!Task 4 (Hard)
+ Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+ 
+ Input: const statement = 'I am a hard working person'
+ 
+ Output:
+ 
+ 'person working hard a am I'
+ * 
+ * 
+*/
+const statement = 'I am a hard working person'
+
+var emptyarray=[]
+ 
+let newStatement = statement.split(' ')
+
+console.log(newStatement)
+
+for( let i = newStatement.length-1; i>=0; i--){
+    const addValue = newStatement[i]
+    emptyarray.push(addValue)
+}
+console.log(emptyarray.join(' '))
